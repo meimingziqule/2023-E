6328675b51e448821eec82be9dcc48780c664323
meimingziqule <132241172+meimingziqule@users.noreply.github.com>
2024-07-09 01:54:54 +0000

    已实现

--- 基于histogram自动L值.py
+++ 基于histogram自动L值.py
@@ -6,24 +6,49 @@ import time
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)  # 设置为灰度图像格式
 sensor.set_framesize(sensor.SVGA)
-baoguang = 10000
-baoguang_step = 2000
+l_value= 60
+baoguang = 40000
+baoguang_step = 3000
 clock = time.clock()
-
+sensor.set_auto_exposure(False,baoguang)#设置感光度  这里至关重要
+auto_exposure_flag = True
+auto_exposure_first = True
 while(True):
     clock.tick()
 
     # 捕获图像
     img = sensor.snapshot()
-    
     # 计算图像的直方图
     histogram = img.histogram()
     histogram_statistics = histogram.get_statistics()
-    print(histogram_statistics)
+    #print(histogram_statistics)
+    if auto_exposure_first:
+        for i in range(20):
+            img = sensor.snapshot()
+            # 计算图像的直方图
+            histogram = img.histogram()
+            histogram_statistics = histogram.get_statistics()    
+            # 计算图像的直方图
+            histogram = img.histogram()
+            histogram_statistics = histogram.get_statistics()
+            # 提取 mode 值
+            if hasattr(histogram_statistics, "mode"):
+                mode_value = histogram_statistics.mode()  # 调用 mode 方法
+                print("mode 值:", mode_value)
+            else:
+                print("histogram_statistics 对象没有 mode 方法")
+        
+            if mode_value > 80:
+                baoguang -= baoguang_step
+                sensor.set_auto_exposure(False,baoguang)#设置感光度  这里至关重要
+                print("亮度减小")
     
-    # 提取 median 值
-    if hasattr(histogram_statistics, "median"):
-        median_value = histogram_statistics.median()  # 调用 median 方法
-        print("Median 值:", median_value)
-    else:
-        print("histogram_statistics 对象没有 median 方法")
+            elif mode_value < 60:
+                baoguang += baoguang_step 
+                sensor.set_auto_exposure(False,baoguang)#设置感光度  这里至关重要
+                print("亮度增大")
+            else:
+                auto_exposure_first = False
+    print("调节已结束")           
+            
+    
--- 自动调节环境的L值.py
+++ 自动调节环境的L值.py
@@ -4,14 +4,38 @@ import time
 
 # 初始化摄像头传感器
 sensor.reset()
-sensor.set_pixformat(sensor.RGB565)
+sensor.set_pixformat(sensor.RGB565)  # 设置为灰度图像格式
 sensor.set_framesize(sensor.SVGA)
+l_value= 60
 baoguang = 10000
 baoguang_step = 2000
 clock = time.clock()
-
+sensor.set_auto_exposure(False,baoguang)#设置感光度  这里至关重要
+auto_exposure_flag = True
 while(True):
     clock.tick()
 
     # 捕获图像
     img = sensor.snapshot()
+    # 计算图像的直方图
+    histogram = img.histogram()
+    histogram_statistics = histogram.get_statistics()
+    print(histogram_statistics)
+    
+    # 提取 median 值
+    if hasattr(histogram_statistics, "median"):
+        median_value = histogram_statistics.median()  # 调用 median 方法
+        print("Median 值:", median_value)
+    else:
+        print("histogram_statistics 对象没有 median 方法")
+    while(auto_exposure_flag):
+        if median_value > 60:
+            baoguang -= baoguang_step
+            sensor.set_auto_exposure(False,baoguang)#设置感光度  这里至关重要
+
+        elif median_value < 60:
+            baoguang += baoguang_step 
+            sensor.set_auto_exposure(False,baoguang)#设置感光度  这里至关重要
+        else:
+            auto_exposure_flag = False
+
