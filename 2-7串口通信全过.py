b8f34267f806121d02faa0fe959eeed71649ef0a
Your Name <307646281@qq.com>
2024-07-12 10:01:38 +0800

    测试

--- 2-7串口通信全过.py
+++ 2-7串口通信全过.py
@@ -3,7 +3,7 @@ from pyb import UART, LED,Pin, Timer
 # 50kHz pin6 timer2 channel1
 #light = Timer(2, freq=50000).channel(1, Timer.PWM, pin=Pin("P6"))
 #light.pulse_width_percent(50) # 控制亮度 0~100
-
+#11
 red_thresholds = (0, 100, 8, 89, -3, 65)# 通用红色阈值
 green_thresholds = (0, 100, 5, 127, -61, 122)# 通用绿色阈值   待修改
 sensor.reset()
